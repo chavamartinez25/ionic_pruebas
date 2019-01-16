91cae55bce7a18b7e933300cb4d2ea452a3291d5
Spartan <ing.salvador.itic@gmail.com>
2019-01-15 22:59:33 -0600

    Lista llenada desde los datos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ANIMALES } from '../../app/data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  animales: any[] = [];
+    constructor() {
+      this.animales = ANIMALES.splice(0);
+    }
 }
